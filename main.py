915f22a2176a2c486e09ed8126dd3f52e9dd51a7
Kracav4ik <dikama2013@yandex.ru>
2016-03-20 22:53:37 +0300

    Кана: главное окно на PyQt5

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 
 from __future__ import division
 
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('kana.ui', self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    main_window = MainWindow()
+    main_window.show()
+
+    sys.exit(app.exec())
