d2a9d5f94ea69f910b940bed2fdd6306405a235c
Kracav4ik <dikama2013@yandex.ru>
2016-03-21 02:58:35 +0300

    Кана: рандомный иероглиф по кнопке

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from __future__ import division
 
 import sys
+import random
 
 from PyQt5.QtCore import QRectF, pyqtSlot
 from PyQt5.QtGui import QPixmap
@@ -10,14 +11,63 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphics
 from PyQt5 import uic
 
 
-class MagicalRectItem(QGraphicsRectItem):
-    def __init__(self, pix, x, y, w, h, tex_x, tex_y):
-        super().__init__(x, y, w, h)
-        self.tex_rect = QRectF(tex_x, tex_y, w, h)
+kana_map = (
+    ('N', 'WA', 'RA', 'YA', 'MA', 'HA', 'NA', 'TA',  'SA',  'KA', 'A'),
+    ('',  'WI', 'RI', '',   'MI', 'HI', 'NI', 'CHI', 'SHI', 'KI', 'I'),
+    ('',  '',   'RU', 'YU', 'MU', 'FU', 'NU', 'TSU', 'SU',  'KU', 'U'),
+    ('',  'WE', 'RE', '',   'ME', 'HE', 'NE', 'TE',  'SE',  'KE', 'E'),
+    ('',  'WO', 'RO', 'YO', 'MO', 'HO', 'NO', 'TO',  'SO',  'KO', 'O'),
+)
+
+
+def get_kana(x, y):
+    return kana_map[y][x]
+
+
+def has_kana(x, y):
+    return 0 <= y < len(kana_map) and 0 <= x < len(kana_map[y]) and get_kana(x, y)
+
+
+kana_list = [(x, y) for x in range(11) for y in range(5) if has_kana(x, y)]
+
+
+class MagicalKanaRect(QGraphicsRectItem):
+    def __init__(self, pix):
+        super().__init__(0, 0, kana_w, kana_h)
+        self.tex_rect = QRectF(0, 0, kana_w, kana_h)
+        self.kana = ''
         self.pix = pix
 
     def paint(self, painter, option, widget=None):
         painter.drawPixmap(self.rect(), self.pix, self.tex_rect)
+
+    def set_kana(self, x, y, is_kata):
+        if not has_kana(x, y):
+            return
+        self.kana = get_kana(x, y)
+
+        if is_kata:
+            gap = kata_gap
+            x0 = kata_x0
+            y0 = kata_y0
+        else:
+            gap = hira_gap
+            x0 = hira_x0
+            y0 = hira_y0
+
+        self.tex_rect = QRectF(x0 + x * kana_w, y0 + y * gap, kana_w, kana_h)
+        self.update()
+
+kana_w = 111
+kana_h = 100
+
+kata_gap = 140
+kata_x0 = 78
+kata_y0 = 318
+
+hira_gap = 138
+hira_x0 = 1222
+hira_y0 = 1190
 
 
 class MainWindow(QMainWindow):
@@ -30,24 +80,8 @@ class MainWindow(QMainWindow):
 
         self.scene = QGraphicsScene()
 
-        kana_w = 111
-        kana_h = 100
-
-        kata_gap = 140
-        kata_x = 78
-        kata_y = 318
-
-        hira_gap = 138
-        hira_x = 1222
-        hira_y = 1190
-
-        for x in range(0, 11):
-            for y in range(5):
-                tex_x = kata_x + x * kana_w
-                tex_y = kata_y + y * kata_gap
-                self.scene.addItem(MagicalRectItem(self.kana, x * kana_w, y * kana_h, kana_w, kana_h, tex_x, tex_y))
-                break
-            break
+        self.kana_rect = MagicalKanaRect(self.kana)
+        self.scene.addItem(self.kana_rect)
         self.graphicsView.setScene(self.scene)
 
         self.hiraCheck.clicked.connect(self.on_kana_check_clicked)
@@ -59,6 +93,12 @@ class MainWindow(QMainWindow):
         if not self.hiraCheck.isChecked() and not self.kataCheck.isChecked():
             other = self.hiraCheck if sender == self.kataCheck else self.kataCheck
             other.setChecked(True)
+
+    @pyqtSlot()
+    def on_nextButton_clicked(self):
+        x, y = random.choice(kana_list)
+        self.kana_rect.set_kana(x, y, random.choice([True, False]))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
