6fa0ce3ea3a86dfd6ce2b556753f6fe3b1ed52e1
Kracav4ik <dikama2013@yandex.ru>
2016-03-21 00:08:32 +0300

    Кана: один иероглиф печатается

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ from __future__ import division
 
 import sys
 
+from PyQt5.QtCore import QRectF
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsRectItem
 from PyQt5 import uic
+
+
+class MagicalRectItem(QGraphicsRectItem):
+    def __init__(self, pix, x, y, w, h, tex_x, tex_y):
+        super().__init__(x, y, w, h)
+        self.tex_rect = QRectF(tex_x, tex_y, w, h)
+        self.pix = pix
+
+    def paint(self, painter, option, widget=None):
+        painter.drawPixmap(self.rect(), self.pix, self.tex_rect)
 
 
 class MainWindow(QMainWindow):
@@ -18,7 +29,8 @@ class MainWindow(QMainWindow):
         self.kana = QPixmap('data\\kana.png')
 
         self.scene = QGraphicsScene()
-        self.scene.addPixmap(self.kana)
+        self.rectitem = MagicalRectItem(self.kana, 0, 0, 111, 100, 411, 318)
+        self.scene.addItem(self.rectitem)
         self.graphicsView.setScene(self.scene)
 
 if __name__ == '__main__':
